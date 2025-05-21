125c4c42bea29a30b2dd27c2b09a947e7ef1e31f
Arth-Haziro <arthurhaziro06@gmail.com>
2025-05-21 00:20:43 +0000

    Defines adicionados.

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,10 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+#define BISPO_MOV 5
+#define TORRE_MOV 5
+#define RAINHA_MOV 8
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
